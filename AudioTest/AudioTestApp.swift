a4c25397188ce4784e39f40169f6424dae25c182
jloborod <jloborod@gmail.com>
2025-04-20 18:56:13 +0100

    Initial Commit

---
+++ AudioTest/AudioTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioTestApp.swift
+//  AudioTest
+//
+//  Created by Joaquín Lobo Rodríguez on 20/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioTest
+//
+//  Created by Joaquín Lobo Rodríguez on 20/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
